7e5dede5b106c07d61b540aac3d93eecf8450f7a
Dong Park <dpark032@wch132-43.cs.ucr.edu>
2019-01-22 16:24:06 -0800

    Implemented world position in camera

--- proj-rt/proj-rt-files/grading/render_world.cpp
+++ proj-rt/proj-rt-files/grading/render_world.cpp
@@ -29,8 +29,7 @@ Hit Render_World::Closest_Intersection(const Ray& ray)
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
-    // TODO;  set up the initial view ray here
-
+    TODO; // set up the initial view ray here
     Ray ray;
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
