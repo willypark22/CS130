8c5fbd7da9fef02392678a7f0a0d020029960326
Dong Park <dpark032@wch132-43.cs.ucr.edu>
2019-01-22 16:42:16 -0800

    Completed part of render pixel

--- proj-rt/proj-rt-files/render_world.cpp
+++ proj-rt/proj-rt-files/render_world.cpp
@@ -30,8 +30,9 @@ Hit Render_World::Closest_Intersection(const Ray& ray)
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
     // TODO;  set up the initial view ray here
-
     Ray ray;
+    vec3 end_point = camera.position;
+    vec3 direction = 
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
 }
