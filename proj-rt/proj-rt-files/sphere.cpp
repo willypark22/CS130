90d384931e1fa8d9162ea0548ed68327ca5cdbcb
willypark22 <dpark032@ucr.edu>
2019-01-23 02:22:58 -0800

    updated intersection

--- proj-rt/proj-rt-files/sphere.cpp
+++ proj-rt/proj-rt-files/sphere.cpp
@@ -4,7 +4,28 @@
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    TODO;
+    //TODO;
+    vec3 u = ray.direction;
+    vec3 v = ray.endpoint - center;
+    
+    float a = 1; //u dot u = 1
+    float b = 2 * dot(u, v);
+    float c = dot(v, v) - (radius * radius);
+    double value = b*b - 4*a*c;
+    
+    float t0 = (-b - sqrt(value)) / (2*a);
+    float t1 = (-b + sqrt(value)) / (2*a);
+        
+    Hit hit0, hit1;
+    
+    if(value < 0) {
+        hit0.object = NULL;
+    }
+    else if (value == 0) {
+        if(
+        hit0.dist = t0;
+        
+    
     return {0,0,0};
 }
 
