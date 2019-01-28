9782625826d4cd665ec6324aa34d0c9da0cd0de3
Dong Park <dpark032@bolt.cs.ucr.edu>
2019-01-28 12:13:54 -0800

    20 points so far for checkpoint 1

--- proj-rt/proj-rt-files/phong_shader.cpp
+++ proj-rt/proj-rt-files/phong_shader.cpp
@@ -8,21 +8,21 @@ vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-	vec3 color, colorAmbient, colorDiffuse, colorSpecular;
+	vec3 color;
 	//TODO; determine the color
 	vec3 l, r, lightSource, intersected;
 	color = world.ambient_color * world.ambient_intensity * color_ambient;
-	for(unsigned i = 0; i < world.lights.size(); ++i) {
-		/*l = world.lights[i]->position - intersection_point;
+	/*for(unsigned i = 0; i < world.lights.size(); ++i) {
+		l = world.lights[i]->position - intersection_point;
 		color += color_diffuse * world.lights[i]->Emitted_Light(l) * std::max(dot(normal, l.normalized()), 0.0);
 		r = -l + 2 * (dot(l, normal)) * normal;
-		color += color_specular * world.lights[i]->Emitted_Light(l) * std::pow(std::max(dot(ray.direction, r.normalized()), 0.0), specular_power);*/
+		color += color_specular * world.lights[i]->Emitted_Light(l) * std::pow(std::max(dot(ray.direction, r.normalized()), 0.0), specular_power);
 		
 		l = world.lights[i]->Emitted_Light(ray);
 		lightSource = world.lights[i]->position;
 		intersected = intersection_point - lightSource;
 		r = -l + 2 * (dot(l, normal) * normal;
 		r = r.normalized();
-	}
+	}*/
 	return color;
 }
